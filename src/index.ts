1e6d82de5da4e9a1357cd0c7b8d66d35487854a2
Gil Hanan 🧐 <gil.hanan@microsoft.com>
2022-07-01 17:47:39 +0300

    Request from JSON file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
+import https from 'https';
 import axios from 'axios';
+import requestsFile from './requests.json';
+
+const instance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  }),
+  validateStatus: (): boolean => true
+});
+
+const summarizeByStatus: { [status: string]: number } = {};
 
 async function main() {
-  const res = await axios.get('http://google.com/');
+  const { Rows } = requestsFile;
+  const { length } = Rows;
 
-  console.log(res.status);
+  for (let index = 0; index < length; index++) {
+    const row = Rows[index];
+
+    const [url] = row;
+
+    console.log(url);
+
+    try {
+      const { status } = await instance.get(url);
+
+      summarizeByStatus[status] = (summarizeByStatus[status] || 0) + 1;
+    } catch (error) {
+      summarizeByStatus['Exceptions'] =
+        (summarizeByStatus['Exceptions'] || 0) + 1;
+      console.error(error);
+    }
+
+    console.log({ length, summarizeByStatus });
+  }
 }
 
 main();
---
+++ src/types/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'requests.json' {
+  const value: {
+    Rows: [string][];
+  };
+  export default value;
+}
